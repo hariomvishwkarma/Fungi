cafdbd26824c7276017c435527701dfe01867c69
hariomvishwakarma1 <117162788+hariomvishwkarma@users.noreply.github.com>
2023-02-01 12:05:21 +0530

    update

--- assets/style.js
+++ assets/style.js
@@ -16,6 +16,7 @@ $(window).on('load', function() {
     $(".loaderMain").fadeOut(2000);
 
 });
+// -------------------------------------------------------
 
 // 3d hover 
 let el = document.getElementById('tilt')
@@ -49,13 +50,13 @@ function handleMove(e) {
      * Control the rotation
      * You can change the value and see the results
      */
-    const yRotation = 20 * ((xVal - width / 2) / width)
+    const yRotation = 20 * ((xVal - width / 5) / width)
 
     /* Calculate the rotation along the X-axis */
-    const xRotation = -20 * ((yVal - height / 2) / height)
+    const xRotation = -20 * ((yVal - height / 5) / height)
 
     /* Generate string for CSS transform property */
-    const string = 'perspective(500px) scale(1.1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+    const string = 'perspective(500px) scale(1.0) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
 
     /* Apply the calculated transformation */
     el.style.transform = string
@@ -68,13 +69,15 @@ el.addEventListener('mouseout', function() {
 
 /* Add listener for mousedown event, to simulate click */
 el.addEventListener('mousedown', function() {
-    el.style.transform = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)'
+    el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
 })
 
 /* Add listener for mouseup, simulate release of mouse click */
 el.addEventListener('mouseup', function() {
-    el.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
+    el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
 })
+
+// ------------------------------------------------------------------------------------------
 
 
 // portfolio jQuery
